3047ec667681e793e951ab98f165daf858db27ce
ZhaoRuiLong <zrl412@163.com>
2019-06-25 14:21:10 +0800

    feat: merge

--- packages/table/src/util.js
+++ packages/table/src/util.js
@@ -175,13 +175,11 @@ export function parseHeight(height) {
     return height;
   }
   if (typeof height === 'string') {
-    if (/^\d+$/.test(height) || /^\d+px/.test(height)) {
+    if (/^\d+(?:px)?$/.test(height)) {
       return parseInt(height, 10);
-    }
-    if (/^\d+%/.test(height)) {
+    } else {
       return height;
     }
-    console.warn('[Element Warn][ElTable]invalid height and it will be ignored.');
   }
   return null;
 }
